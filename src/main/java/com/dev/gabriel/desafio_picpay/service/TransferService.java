fe3b8482e703c86f8bb498a3e84e21bcd8970d14
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-30 19:45:48 -0300

    feat: implementing method to validate a transfer

--- src/main/java/com/dev/gabriel/desafio_picpay/service/TransferService.java
+++ src/main/java/com/dev/gabriel/desafio_picpay/service/TransferService.java
@@ -1,5 +1,10 @@
 package com.dev.gabriel.desafio_picpay.service;
 
+import com.dev.gabriel.desafio_picpay.dto.CreateTransferRequest;
+import com.dev.gabriel.desafio_picpay.exception.InsufficientBalanceException;
+import com.dev.gabriel.desafio_picpay.exception.TransferNotAllowedException;
+import com.dev.gabriel.desafio_picpay.model.entity.Wallet;
+import com.dev.gabriel.desafio_picpay.model.enums.WalletType;
 import com.dev.gabriel.desafio_picpay.repository.TransferRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +14,20 @@ import org.springframework.stereotype.Service;
 public class TransferService {
   private final TransferRepository transferRepository;
   private final WalletService walletService;
+  private final AuthorizationService authorizationService;
+
+  public void validateTransfer(CreateTransferRequest request, Wallet sender) {
+    if (sender.getWalletType() != WalletType.COMMON_USER) {
+      throw new TransferNotAllowedException(
+          "Seu tipo de carteira não permite a realização de transferências");
+    }
+
+    if (sender.getBalance().doubleValue() < request.value().doubleValue()) {
+      throw new InsufficientBalanceException("O saldo é insuficiente");
+    }
+
+    if (!this.authorizationService.isAuthorized(request)) {
+      throw new TransferNotAllowedException("A transferência não foi autorizada");
+    }
+  }
 }
