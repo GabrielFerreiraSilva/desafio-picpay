936f257f2215f6247a36b509ff4bee01a9aab998
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-30 14:59:56 -0300

    feat: implementing an exception handler to Validation exceptions

--- src/main/java/com/dev/gabriel/desafio_picpay/controller/CustomExceptionHandler.java
+++ src/main/java/com/dev/gabriel/desafio_picpay/controller/CustomExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.dev.gabriel.desafio_picpay.controller;
 
 import com.dev.gabriel.desafio_picpay.exception.PicPayException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
@@ -12,4 +16,21 @@ public class CustomExceptionHandler {
   public ProblemDetail handlePicPayException(PicPayException e) {
     return e.generateProblemDetail();
   }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ProblemDetail handleValidationException(MethodArgumentNotValidException e) {
+
+    List<InvalidParam> fieldErrors =
+        e.getFieldErrors().stream()
+            .map(error -> new InvalidParam(error.getField(), error.getDefaultMessage()))
+            .toList();
+
+    ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+    problemDetail.setTitle("Request parameters did not validate");
+    problemDetail.setProperty("Parâmetros inválidos", fieldErrors);
+
+    return problemDetail;
+  }
+
+  private record InvalidParam(String name, String reason) {}
 }
