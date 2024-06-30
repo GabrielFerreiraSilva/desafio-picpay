85fbc63a13ce88ff21104cd53d9923f196ee3e27
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-30 19:18:20 -0300

    chore: implementing authorization client interface

---
+++ src/main/java/com/dev/gabriel/desafio_picpay/client/AuthorizationClient.java
@@ -0,0 +1,15 @@
+package com.dev.gabriel.desafio_picpay.client;
+
+import com.dev.gabriel.desafio_picpay.dto.AuthorizationClientResponse;
+import com.dev.gabriel.desafio_picpay.model.entity.Transfer;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "authorization_client", url = "${client.authorization-service.url}")
+public interface AuthorizationClient {
+
+  @GetMapping
+  ResponseEntity<AuthorizationClientResponse> isAuthorized(@RequestBody Transfer transfer);
+}
