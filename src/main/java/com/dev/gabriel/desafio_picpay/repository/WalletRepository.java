ff459c24c4195028d570f656334f5a53ec7450d6
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-30 14:46:11 -0300

    feat: creating a query method to verify if there is a wallet by cpf/cnpj or email

--- src/main/java/com/dev/gabriel/desafio_picpay/repository/WalletRepository.java
+++ src/main/java/com/dev/gabriel/desafio_picpay/repository/WalletRepository.java
@@ -3,4 +3,6 @@ package com.dev.gabriel.desafio_picpay.repository;
 import com.dev.gabriel.desafio_picpay.model.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface WalletRepository extends JpaRepository<Wallet, Long> {}
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+  boolean existsByCpfCnpjOrEmail(String cpfCnpj, String email);
+}
