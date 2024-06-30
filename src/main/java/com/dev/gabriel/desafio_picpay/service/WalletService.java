9584c66fa4f3d55cd8fc0308ce4054d5e545c687
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-30 14:46:31 -0300

    feat: creating a method to create a wallet

--- src/main/java/com/dev/gabriel/desafio_picpay/service/WalletService.java
+++ src/main/java/com/dev/gabriel/desafio_picpay/service/WalletService.java
@@ -1,5 +1,9 @@
 package com.dev.gabriel.desafio_picpay.service;
 
+import com.dev.gabriel.desafio_picpay.dto.CreateWalletRequest;
+import com.dev.gabriel.desafio_picpay.dto.WalletResponse;
+import com.dev.gabriel.desafio_picpay.exception.WalletAlreadyRegisteredException;
+import com.dev.gabriel.desafio_picpay.model.entity.Wallet;
 import com.dev.gabriel.desafio_picpay.repository.WalletRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +12,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class WalletService {
   private final WalletRepository walletRepository;
+
+  public WalletResponse createWallet(CreateWalletRequest request) {
+
+    if (this.walletRepository.existsByCpfCnpjOrEmail(request.cpfCnpj(), request.email())) {
+      throw new WalletAlreadyRegisteredException("O CPF/CNPJ ou email fornecidos já estão em uso");
+    }
+
+    Wallet savedWallet = this.walletRepository.save(new Wallet(request));
+
+    return new WalletResponse(savedWallet);
+  }
 }
