ca6780df38fd30e22c2541174c4f19133f6e50b1
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-30 14:22:28 -0300

    feat: adding JPA annotations to wallet entity class

--- src/main/java/com/dev/gabriel/desafio_picpay/model/entity/Wallet.java
+++ src/main/java/com/dev/gabriel/desafio_picpay/model/entity/Wallet.java
@@ -1,15 +1,34 @@
 package com.dev.gabriel.desafio_picpay.model.entity;
 
 import com.dev.gabriel.desafio_picpay.model.enums.WalletType;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "tb_wallet")
 public class Wallet {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
+  @Column(nullable = false)
   private String name;
+
+  @Column(nullable = false, unique = true)
   private String cpfCnpj;
+
+  @Column(nullable = false, unique = true)
   private String email;
+
+  @Column(nullable = false)
   private String password;
+
+  @Column(nullable = false)
   private BigDecimal balance = BigDecimal.ZERO;
+
+  @Column(nullable = false)
+  @Enumerated(EnumType.STRING)
   private WalletType walletType;
 }
