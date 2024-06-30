60ad5df433eeee23a03f885621ad2c2a61533175
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-30 14:49:57 -0300

    feat: implementing in controller layer a method to create a wallet

--- src/main/java/com/dev/gabriel/desafio_picpay/controller/WalletController.java
+++ src/main/java/com/dev/gabriel/desafio_picpay/controller/WalletController.java
@@ -1,7 +1,13 @@
 package com.dev.gabriel.desafio_picpay.controller;
 
+import com.dev.gabriel.desafio_picpay.dto.CreateWalletRequest;
+import com.dev.gabriel.desafio_picpay.dto.WalletResponse;
 import com.dev.gabriel.desafio_picpay.service.WalletService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class WalletController {
   private final WalletService walletService;
+
+  @PostMapping
+  public ResponseEntity<WalletResponse> createWallet(
+      @RequestBody @Valid CreateWalletRequest request) {
+
+    WalletResponse response = this.walletService.createWallet(request);
+
+    return ResponseEntity.ok(response);
+  }
 }
