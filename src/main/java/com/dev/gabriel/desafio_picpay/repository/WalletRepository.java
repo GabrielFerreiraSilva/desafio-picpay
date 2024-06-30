50086caefb46db9e6ad2cf6b41c3464736a5cd22
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-30 14:34:57 -0300

    feat: creating a wallet repository interface

---
+++ src/main/java/com/dev/gabriel/desafio_picpay/repository/WalletRepository.java
@@ -0,0 +1,6 @@
+package com.dev.gabriel.desafio_picpay.repository;
+
+import com.dev.gabriel.desafio_picpay.model.entity.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WalletRepository extends JpaRepository<Wallet, Long> {}
