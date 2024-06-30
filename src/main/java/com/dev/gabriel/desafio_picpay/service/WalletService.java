dc9b89cf438ae384038f0c1511c6fc61f34e53c2
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-30 19:57:26 -0300

    feat: implementing method to credit a value to a wallet

--- src/main/java/com/dev/gabriel/desafio_picpay/service/WalletService.java
+++ src/main/java/com/dev/gabriel/desafio_picpay/service/WalletService.java
@@ -3,16 +3,21 @@ package com.dev.gabriel.desafio_picpay.service;
 import com.dev.gabriel.desafio_picpay.dto.CreateWalletRequest;
 import com.dev.gabriel.desafio_picpay.dto.WalletResponse;
 import com.dev.gabriel.desafio_picpay.exception.WalletAlreadyRegisteredException;
+import com.dev.gabriel.desafio_picpay.exception.WalletNotFoundException;
 import com.dev.gabriel.desafio_picpay.model.entity.Wallet;
 import com.dev.gabriel.desafio_picpay.repository.WalletRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
 public class WalletService {
   private final WalletRepository walletRepository;
 
+  @Transactional
   public WalletResponse createWallet(CreateWalletRequest request) {
 
     if (this.walletRepository.existsByCpfCnpjOrEmail(request.cpfCnpj(), request.email())) {
@@ -22,5 +27,14 @@ public class WalletService {
     Wallet savedWallet = this.walletRepository.save(new Wallet(request));
 
     return new WalletResponse(savedWallet);
+  }
+
+  @Transactional
+  public void credit(Long id, BigDecimal value) {
+    Wallet wallet = this.walletRepository.findById(id).orElseThrow(WalletNotFoundException::new);
+
+    wallet.setBalance(wallet.getBalance().add(value));
+
+    this.walletRepository.save(wallet);
   }
 }
