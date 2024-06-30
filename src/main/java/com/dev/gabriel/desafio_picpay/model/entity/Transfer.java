fd06b0e96fc11a1dc0df63a059d98c67a8a5f14d
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-30 15:06:59 -0300

    feat: adding JPA annotations to Transfer entity class

--- src/main/java/com/dev/gabriel/desafio_picpay/model/entity/Transfer.java
+++ src/main/java/com/dev/gabriel/desafio_picpay/model/entity/Transfer.java
@@ -1,11 +1,28 @@
 package com.dev.gabriel.desafio_picpay.model.entity;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Entity
+@Table(name = "tb_transfer")
 public class Transfer {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Column(nullable = false)
+  @ManyToOne
+  @JoinColumn(name = "sender_id")
   private Wallet sender;
+
+  @Column(nullable = false)
+  @ManyToOne
+  @JoinColumn(name = "receiver_id")
   private Wallet receiver;
+
+  @Column(nullable = false)
   private BigDecimal value;
 }
