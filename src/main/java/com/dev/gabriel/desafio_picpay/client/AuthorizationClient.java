8e478a6fe6a028fb21e2a9761c63828defdc863d
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-30 23:00:26 -0300

    fix: fix feign client name error

--- src/main/java/com/dev/gabriel/desafio_picpay/client/AuthorizationClient.java
+++ src/main/java/com/dev/gabriel/desafio_picpay/client/AuthorizationClient.java
@@ -2,13 +2,12 @@ package com.dev.gabriel.desafio_picpay.client;
 
 import com.dev.gabriel.desafio_picpay.dto.AuthorizationClientResponse;
 import com.dev.gabriel.desafio_picpay.dto.CreateTransferRequest;
-import com.dev.gabriel.desafio_picpay.model.entity.Transfer;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "authorization_client", url = "${client.authorization-service.url}")
+@FeignClient(name = "authorization-client", url = "${client.authorization-service.url}")
 public interface AuthorizationClient {
 
   @GetMapping
