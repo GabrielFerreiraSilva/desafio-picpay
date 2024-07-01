2100784b0257754624c454e5319ce00fedd16d52
GabrielFerreiraSilva <gabrielfsilva936@gmail.com>
2024-06-30 21:46:20 -0300

    feat: implementing method to creating a transfer in controller layer

--- src/main/java/com/dev/gabriel/desafio_picpay/controller/TransferController.java
+++ src/main/java/com/dev/gabriel/desafio_picpay/controller/TransferController.java
@@ -1,7 +1,13 @@
 package com.dev.gabriel.desafio_picpay.controller;
 
+import com.dev.gabriel.desafio_picpay.dto.CreateTransferRequest;
+import com.dev.gabriel.desafio_picpay.dto.TransferResponse;
 import com.dev.gabriel.desafio_picpay.service.TransferService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TransferController {
   private final TransferService transferService;
+
+  @PostMapping
+  public ResponseEntity<TransferResponse> transfer(
+      @RequestBody @Valid CreateTransferRequest request) {
+    TransferResponse response = this.transferService.createTransfer(request);
+    return ResponseEntity.ok(response);
+  }
 }
